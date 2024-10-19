dfa7c0c2771703ffee2661f68a404e21eac0131f
Krunoslav Husak <khusak@vub.hr>
2024-10-19 11:28:37 +0000

    Korištenje klase Motocikl

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 #include "motocikl.h"
 
 int main() {
-  Motocikl m1;
+  Motocikl motor1;
+  motor1.setProizvodac("Yamaha");
+  motor1.setModel("FZ6");
+  motor1.setZapremnina(600);
+  motor1.info();
+
+  Motocikl motor2("Honda", "CBR600RR");
+  motor2.info();
+
+  Motocikl motor3("Suzuki", "GSX-R1000", 1000);
+  motor3.info();
 }

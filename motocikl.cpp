994cf30c1aa094bea67956a21cf322da015fd2d3
Krunoslav Husak <khusak@vub.hr>
2024-10-19 11:16:30 +0000

    Implementirani getteri i setteri

--- motocikl.cpp
+++ motocikl.cpp
@@ -1,3 +1,30 @@
 #include "motocikl.h"
 
 Motocikl::Motocikl() : proizvodac(""), model(""), zapremnina(0) {}
+
+std::string Motocikl::getProizvodac() {
+    return proizvodac;
+}
+
+std::string Motocikl::getModel() {
+    return model;
+}
+
+int Motocikl::getZapremnina() {
+    return zapremnina;
+}
+
+void Motocikl::setProizvodac(std::string proizvodac) {
+    this->proizvodac = proizvodac;
+}
+
+void Motocikl::setModel(std::string model) {
+    this->model = model;
+}
+
+void Motocikl::setZapremnina(int zapremnina) { // zapremnina mora biti pozitivan broj i ne veći od 2000
+    if (zapremnina <= 0 || zapremnina > 2000) {
+        return;
+    }
+    this->zapremnina = zapremnina;
+}
--- motocikl.h
+++ motocikl.h
@@ -4,6 +4,14 @@ class Motocikl {
 public:
     Motocikl();
 
+    std::string getProizvodac();
+    std::string getModel();
+    int getZapremnina();
+
+    void setProizvodac(std::string proizvodac);
+    void setModel(std::string model);
+    void setZapremnina(int zapremnina);
+
 private:
     std::string proizvodac;
     std::string model;
